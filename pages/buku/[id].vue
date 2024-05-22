03a1f2d769bc9a10b1d603943f7fa1ff5b60f6af
Afkar-Saga <afkarsukmawanahmad@gmail.com>
2024-05-22 10:36:20 +0700

    improve filter buku, detail buku

--- pages/buku/[id].vue
+++ pages/buku/[id].vue
@@ -6,32 +6,42 @@
       </NuxtLink>
     </div>
     <div class="container">
-      <div class="row justify-content-center">
+      <div v-if="status == 'pending'" class="row justify-content-center">
+        <div class="col">Loading...</div>
+      </div>
+      <div v-if="status == 'error'" class="row justify-content-center">
+        <div class="col">{{ error.message }}</div>
+      </div>
+      <div v-if="status == 'success'" class="row justify-content-center">
         <div class="col-8 col-lg-4 mb-3">
           <div class="card shadow-lg rounded-5">
-            <img src="/assets/img/book-bumi.png" alt="" class="card-img-top p-4">
+            <img :src="book.coverUrl.publicUrl" alt="" class="card-img-top p-4">
           </div>
         </div>
         <div class="col-12 col-lg-8">
           <div class="row">
             <div class="col mb-3">
-              <h5 class="text-muted">Tere Liye</h5>
-              <h2>Bumi</h2>
+              <h5 class="text-muted">{{ book?.penulis }}</h5>
+              <h2>{{ book?.judul }}</h2>
             </div>
           </div>
           <div class="row">
             <div class="col mb-3">
               <h4 class="fw-bold">Deskripsi Buku</h4>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ad expedita velit, doloremque minus labore. Debitis officia ea consequuntur iure esse ab ut nisi non quis, placeat at impedit quas delectus, aliquam in facilis incidunt accusantium, magnam quod vel minima! Saepe suscipit a cum dolorum debitis dolores eaque ratione explicabo.</p>
+              <p>{{ book?.deskripsi }}</p>
             </div>
           </div>
           <div class="row">
             <div class="col">
               <h4 class="fw-bold">Detail Buku</h4>
               <h6 class="text-muted">Penerbit</h6>
-              <p>SABAKGRIP</p>
+              <p>{{ book?.penerbit }}</p>
               <h6 class="text-muted">Tanggal Terbit</h6>
-              <p>30 Agustus 2022</p>
+              <p>{{ book?.tahun_terbit }}</p>
+            </div>
+            <div class="col-3 bg-info bg-gradient bg-opacity-50 p-3 m-2 rounded shadow-md">
+              <h5 class="text-success fw-bold">Rak</h5>
+              <p class="fst-italic">{{ book?.rak?.kode_rak }} | {{ book?.rak?.nama }}</p>
             </div>
           </div>
         </div>
@@ -40,8 +50,19 @@
   </div>
 </template>
 
-<script lang="ts" setup>
+<script setup>
+const { id } = useRoute().params
+const supabase = useSupabaseClient()
 
+const { data: book, status, error } = useLazyAsyncData('book', async () => {
+  const { data, error } = await supabase.from('buku').select(`*, rak ( kode_rak, nama )`).eq('id', id).maybeSingle()
+  if (error) throw error
+  if (data) {
+    const { data: url } = supabase.storage.from('books').getPublicUrl(`cover/${data.cover || 'book_cover_placeholder.png'}`)
+    data.coverUrl = url
+  }
+  return data
+})
 </script>
 
 <style scoped>
--- pages/buku/index.vue
+++ pages/buku/index.vue
@@ -8,21 +8,23 @@
       </div>
       <div class="row">
         <div class="col">
-          <input type="text" v-model="search" class="form-control" placeholder="Cari buku berdasarkan judul atau pengarang" @input="refresh">
-          {{ search }}
+          <input type="text" v-model="search" class="form-control"
+            placeholder="Cari buku berdasarkan judul atau pengarang" @input="refresh">
         </div>
       </div>
       <div class="row align-items-center my-2">
-        <div class="col-lg-1 col-2">
+        <div class="col-lg-auto col-2">
           <label for="kode">Kode Rak: </label>
         </div>
         <div class="col-lg-3 col-4">
-          <select v-model="shelf" id="kode" class="form-control form-select" @change="refresh">
+          <select v-model="shelf" id="kode" class="form-control form-select"
+            @change="selectShelf">
             <option value="">None</option>
-            <option v-for="shelf in shelves" :key="shelf.id" :value="shelf.id">{{ shelf.kode_rak }} | {{ shelf.nama }}</option>
+            <option v-for="shelf in shelves" :key="shelf.id" :value="shelf.id">{{ shelf.kode_rak }} | {{ shelf.nama }}
+            </option>
           </select>
         </div>
-        <div class="col-lg-1 col-2">
+        <div class="col-lg-auto col-2">
           <label for="kategori">Kategori: </label>
         </div>
         <div class="col-lg-3 col-4">
@@ -31,10 +33,21 @@
             <option v-for="category in categories" :key="category.id" :value="category.id">{{ category.nama }}</option>
           </select>
         </div>
+        <div class="col-lg-auto col-4 ms-auto">
+          <label for="kategori">Buku per Halaman: </label>
+        </div>
+        <div class="col-lg-1 col-2">
+          <select v-model="limit" id="kategori" class="form-control form-select" @change="refresh">
+            <option :value="4">4</option>
+            <option :value="8">8</option>
+            <option :value="16">16</option>
+            <option :value="32">32</option>
+          </select>
+        </div>
       </div>
       <div class="row">
         <div class="col">
-          <p class="text-black-70">Menampilkan {{ books?.length }} dari {{ totalBooks }} buku</p>
+          <p class="text-black-70">Menampilkan {{ books?.length ? `${page * limit + 1}-${page * limit + books?.length}` : 0 }} dari {{ totalBooks }} buku</p>
         </div>
       </div>
       <div class="row gx-2 gy-4 justify-content-evenly">
@@ -50,19 +63,18 @@
       </div>
       <div class="row justify-content-between my-4">
         <div class="col-auto">
-          <button class="btn btn-dark" @click="changePage(-1)">Previous</button>
+          <button class="btn btn-dark" @click="previousPage">Previous</button>
         </div>
         <div class="col-auto" v-if="status == 'pending'">
           Loading...
         </div>
         <div class="col-auto" v-else>
-          {{ page + 1 }}
+          {{ page + 1 }}/{{ pageLimit + 1 }}
         </div>
         <div class="col-auto">
-          <button class="btn btn-dark" @click="changePage(1)">Next</button>
+          <button class="btn btn-dark" @click="nextPage">Next</button>
         </div>
       </div>
-      {{ category }} {{ typeof(category) }} {{ shelf }} {{ typeof shelf }}
     </div>
   </div>
 </template>
@@ -71,9 +83,19 @@
 const supabase = useSupabaseClient()
 
 const page = ref(0)
+const limit = ref(8)
+const pageLimit = computed(() => {
+  return totalBooks.value == 0 ? 0 : Math.ceil(totalBooks.value / limit.value) - 1
+})
 
-const changePage = (where) => {
-  page.value += where
+const nextPage = () => {
+  if (page.value >= pageLimit.value) return
+  page.value += 1
+  refresh()
+}
+const previousPage = () => {
+  if (page.value <= 0) return
+  page.value -= 1
   refresh()
 }
 
@@ -81,13 +103,13 @@ const search = ref('')
 const category = ref('')
 const shelf = ref('')
 
-const { data: books, status, error, refresh } = useLazyAsyncData('books', async () => {
-  const { from, to } = getPagination(page.value, 8)
+const { data: books, status, error, refresh } = useAsyncData('books', async () => {
+  const { from, to } = getPagination(page.value, limit.value)
   let query = supabase.from('buku').select()
   if (search.value) query = query.or(`judul.ilike.%${search.value}%, penulis.ilike.%${search.value}%`)
   if (category.value) query = query.eq('kategori', category.value)
   if (shelf.value) query = query.eq('rak', shelf.value)
-  query = query.range(from, to).order('kategori')
+  query = query.range(from, to)
 
   const { data, error } = await query
   if (error) throw error
@@ -97,18 +119,32 @@ const { data: books, status, error, refresh } = useLazyAsyncData('books', async 
       book.coverUrl = url
     })
   }
-  console.log(data)
   return data
 })
 
 const { data: totalBooks } = useAsyncData('totalBooks', async () => {
-  const { count, error } = await supabase.from('buku').select('*', { count: 'exact', head: true })
+  let query = supabase.from('buku').select('*', { count: 'exact', head: true })
+  if (category.value) query = query.eq('kategori', category.value)
+  if (shelf.value) query = query.eq('rak', shelf.value)
+  const { count, error } = await query
   if (error) throw error
   return count
+}, {
+  watch: [category, shelf]
 })
 
-const { data: categories } = useAsyncData('categories', async () => {
-  const { data, error } = await supabase.from('kategori_buku').select()
+
+const { data: categories, refresh: refreshCategory } = useAsyncData('categories', async () => {
+  let query = supabase.from('kategori_buku').select()
+  if (shelf.value) {
+    const { data, error } = await supabase.from('buku').select('kategori').eq('rak', shelf.value)
+    if (error) throw error
+    if (data) {
+      // let booksCategory = books.value.map(({ kategori }) => kategori)
+      query = query.in('id', data.map(({ kategori }) => kategori))
+    }
+  }
+  const { data, error } = await query
   if (error) throw error
   return data
 })
@@ -118,9 +154,25 @@ const { data: shelves } = useAsyncData('shelves', async () => {
   return data
 })
 
+const selectShelf = () => {
+  refresh()
+    .then(() => refreshCategory())
+    .then(() => {
+      let categoryMap = categories.value.map(({ id }) => id)
+      if (!(categoryMap.includes(category.value))) {
+        category.value = ''
+        refresh()
+      }
+    })
+}
+
 onMounted(() => {
   clearNuxtData('books')
   refresh()
+})
+
+watch([search, category, shelf, limit], () => {
+  page.value = 0;
 })
 </script>
 
